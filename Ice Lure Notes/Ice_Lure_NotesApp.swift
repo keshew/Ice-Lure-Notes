06d216b783bba1cfec8a978ed70ae58ef19aac8e
keshew <artyom.korotckov2017@yandex.ru>
2026-01-13 18:13:03 +0500

    Initial Commit

---
+++ Ice Lure Notes/Ice_Lure_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ice_Lure_NotesApp.swift
+//  Ice Lure Notes
+//
+//  Created by Артём Коротков on 13.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Ice_Lure_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
